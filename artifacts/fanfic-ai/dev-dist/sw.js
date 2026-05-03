10dcb90701d5c02adea2636a98f7a2768558d9b6
ruelasthain473 <57341094-ruelasthain473@users.noreply.replit.com>
2026-05-03 04:55:14 +0000

    Git commit prior to merge

--- artifacts/fanfic-ai/dev-dist/sw.js
+++ artifacts/fanfic-ai/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e12c70bb'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.732vgohu53c"
+    "revision": "0.0qf5tnbp1rk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
